b01f94dd39ce58f356a8a93a22003451b238cc5a
Nasir Mollah <nasirmollah192@gmail.com>
2024-11-05 09:07:34 +0600

    :sparkle: add comment update and add or remove like on comment route added

--- src/app/modules/Comment/comment.routes.ts
+++ src/app/modules/Comment/comment.routes.ts
@@ -2,7 +2,13 @@ import { Router } from "express";
 import { CommentControllers } from "./comment.controller";
 
 const router = Router();
+// create a new comment
 router.post("/create-comment", CommentControllers.createComment);
+// update the comment
 router.patch("/update-comment", CommentControllers.updateComment);
+// add or remove like on comment
+router.patch("/add-remove-like", CommentControllers.addOrRemoveLike);
+// delete the comment
+router.delete("/delete-comment", CommentControllers.deleteComment);
 
 export const commentRoutes = router;
